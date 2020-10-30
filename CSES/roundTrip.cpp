7bafdcc8bd0bb3f08d827f5d88d47719f28b27b8
Mohil Khare <mohilkhare17@gmail.com>
2020-10-30 21:34:48 +0530

    round trip :100:
    getting the hang of dfs

--- CSES/roundTrip.cpp
+++ CSES/roundTrip.cpp
@@ -10,8 +10,7 @@ Created On: 24 October 2020, 18:58
 #define vpll vector<pair<long long int, long long int>>
 #define mp make_pair
 #define pb push_back
-#define endl "\n"
-#define here std::cout << "here\n";
+#define here std::cout << "here" << std::endl;
 #define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL);
 
 // program specific shorts (if any)
@@ -20,16 +19,46 @@ const ll mod = 1000000007;
 using namespace std;
 
 // so basically, find a cycle in an undirected graph
+const ll MAX = 1e5 + 1;
+vll adj[MAX], ans;
+bool vis[MAX];
+ll p[MAX];
+
+void dfs(ll i, ll parent) {
+    p[i] = parent;
+    vis[i] = true;
+    for(auto x : adj[i]) {
+        if(parent == x)
+            continue;
+        if(vis[x]) {
+            ll st = i;
+            while(i ^ x) {
+                ans.pb(i);
+                i = p[i];
+            }
+            ans.pb(x);
+            ans.pb(st);
+            cout << ans.size() << endl;
+            for(auto y : ans) cout << y+1 << " ";
+            exit(0);
+        }
+        else dfs(x, i);
+    }
+}
 
 int main() {
     fastIO;
     ll n, m; cin >> n >> m;
-    vvll adj(n);
     for(ll i = 0; i < m; i++) {
         ll a, b; cin >> a >> b;
         adj[a-1].pb(b-1);
         adj[b-1].pb(a-1);
     }
-    
+    memset(vis, false, sizeof(vis));
+    for(ll i = 0; i < n; i++) {
+        if(!vis[i])
+            dfs(i, -1);
+    }
+    cout << "IMPOSSIBLE";
     return 0;
 }
