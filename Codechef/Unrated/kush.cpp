3d6657937fb6199ab1a9885328e27b898344e726
Mohil Khare <mohilkhare17@gmail.com>
2020-11-13 02:05:41 +0530

    stuff

--- Codechef/Unrated/kush.cpp
+++ Codechef/Unrated/kush.cpp
@@ -1,52 +1,59 @@
-#include <bits/stdc++.h>
-//shorts
-#define ll long long int
-#define sll stack<long long int>
-#define vll vector<long long int>
-#define ld long double
-#define pb push_back
-#define mp make_pair
-#define vpll vector<pair<long long int, long long int>>
-#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define fo(i,x,y) for(long long i = x; i < y; i++)
-#define MOD 1000000007
-#define here std::cout << "here" << std::endl
-#define F first
-#define S second
-#define s(a) a.size()
+/*
+Author : Mohil Khare
+Created On: 13 November 2020, 01:13
+*/
 
-//program specific shorts (if any)
-//
+#include <bits/stdc++.h>
+#define ll long long int
+#define vll vector<long long int>
+#define vvll vector<vector<long long int>>
+#define vpll vector<pair<long long int, long long int>>
+#define mp make_pair
+#define pb push_back
+#define endl "\n"
+#define here std::cout << "here\n";
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+// program specific shorts (if any)
+const ll mod = 1000000007;
+const ll maxN = 4000000;
+
 using namespace std;
 
-ll getlcm(ll a, ll b) {
-    return (a*b)/__gcd(a, b);
-}
+vll primes;
+
+// i attribute this code to gfg Sieve of Eratosthenes : https://www.geeksforgeeks.org/sieve-of-eratosthenes/
+
+void SieveOfEratosthenes() { 
+    bool prime[maxN+1]; 
+    primes.pb(1); // to support 1 based indexing
+    memset(prime, true, sizeof(prime)); 
+    for(int p = 2; p*p <= maxN; p++) { 
+        if(prime[p] == true) { 
+            for (int i= p * p; i <= maxN; i+=p) 
+                prime[i] = false; 
+        } 
+    } 
+    for(int p = 2; p <= maxN; p++) 
+        if (prime[p]) 
+            primes.pb(p);
+} 
+// attribution ends here
 
 int main() {
     fastIO;
     ll t; cin >> t;
-    while(t--) {
-        string str; cin >> str;
-        if(s(str) == 1) {cout << "not balanced\n"; continue;}
-        map<char, char> close;
-        close['('] = ')';
-        close['['] = ']';
-        close['{'] = '}';
-		// here;
-        stack<char> check;
-		// cout << check.top() << endl;
-		// here;
-        fo(i, 0, s(str)) {
-			if(check.empty())
-				continue;
-            if(str[i] == close[check.top()])
-				check.pop();
-            else 
-				check.push(str[i]);
-        }
-        if(check.empty()) cout << "balanced\n";
-        else cout << "not balanced\n";
+    SieveOfEratosthenes();
+    for(ll qq = 0; qq < t; qq++) {
+        ll n; cin >> n;
+        vll b(n+1); for(ll i = 1; i <= n; i++) cin >> b[i];
+        vll ans(n+1);
+        for(ll i = 1; i <= n; i++) ans[i] = primes[i];
+        for(ll i = 1; i <= n; i++)
+            ans[i] = primes[b[i]];
+        for(ll i = 1; i <= n; i++)
+            cout << ans[i] << " ";
+        cout << endl;
     }
     return 0;
 }
